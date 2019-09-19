8d557f438bfc7af51a9ff679d8d2b619a20a6bc2
Tarrke <tarrke@gmail.com>
2019-09-19 16:08:06 +0200

    linting fixes

--- tests/unit/tests.py
+++ tests/unit/tests.py
@@ -1,9 +1,12 @@
+""" Small Unittesting tests """
+
 import unittest
 
 class Tests(unittest.TestCase):
+    """ Small Unittesting tests """
     def test_sum(self):
         """Small sum tests"""
-        self.assertEqual(sum([1,2,3]), 6, "Should be 6")
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
 if __name__ == '__main__':
     unittest.main()
--- tests/unit/tests_index.py
+++ tests/unit/tests_index.py
@@ -1,30 +1,45 @@
+""" Unit tests for wxw_markdowner module """
+
 import unittest
 
-from src.wxwMarkdowner import WXWMarkdowner
+from src.wxw_markdowner import WXWMarkdowner
 
-class indexTests(unittest.TestCase):
+class IndexTests(unittest.TestCase):
+    """ Tests for the index thingy. """
     def setUp(self):
         self.filename = "test"
         self.title = "Dummy Storie"
         self.author = "Me"
 
     def test_generate_filenames(self):
-        myMarkdowner = WXWMarkdowner(self.filename, self.title, "https://www.wuxiaworld.com/novel/martial-world", self.author)
-        myMarkdowner.generate_filenames()
+        """ test generate filename """
+        my_markdowner = WXWMarkdowner(self.filename, self.title,
+                                      "https://www.wuxiaworld.com/novel/martial-world", self.author)
+        my_markdowner.generate_filenames()
 
 
     def test_index_dl(self):
-        myMarkdowner = WXWMarkdowner(self.filename, self.title, "https://www.wuxiaworld.com/novel/martial-world", self.author)
-        myMarkdowner.generate_filenames()
-        myMarkdowner.generate_metadata()
-        myMarkdowner.download_index(["Chapter", "MW", "Afterword"], ["Chapters"])
-        self.assertEqual(myMarkdowner.chaps[-1], (2277, 'http://wuxiaworld.com/novel/martial-world/mw-chapter-2256', 'Afterword'), "Comment")
+        """ test index download """
+        my_markdowner = WXWMarkdowner(self.filename, self.title,
+                                      "https://www.wuxiaworld.com/novel/martial-world", self.author)
+        my_markdowner.generate_filenames()
+        my_markdowner.generate_metadata()
+        my_markdowner.download_index(["Chapter", "MW", "Afterword"], ["Chapters"])
+        self.assertEqual(my_markdowner.chaps[-1],
+                         (2277, 'http://wuxiaworld.com/novel/martial-world/mw-chapter-2256',
+                          'Afterword'), "Comment")
 
     def test_chapter_generation(self):
-        myMarkdowner = WXWMarkdowner(self.filename, self.title, "https://www.wuxiaworld.com/novel/martial-world", self.author)
-        myMarkdowner.get_chapter_from_index("./tests/unit/test_index.html", starts = ["Chapter", "MW", "Afterword"], nostarts = ["Chapters"])
-        # print(myMarkdowner.chaps[-1])
-        self.assertEqual(myMarkdowner.chaps[-1], (738, 'http://wuxiaworld.com/novel/martial-world/mw-chapter-2256', 'Afterword'), "Comment")
+        """ test chapter generation """
+        my_markdowner = WXWMarkdowner(self.filename, self.title,
+                                      "https://www.wuxiaworld.com/novel/martial-world", self.author)
+        my_markdowner.get_chapter_from_index("./tests/unit/test_index.html",
+                                             starts=["Chapter", "MW", "Afterword"],
+                                             nostarts=["Chapters"])
+        # print(my_markdowner.chaps[-1])
+        self.assertEqual(my_markdowner.chaps[-1],
+                         (738, 'http://wuxiaworld.com/novel/martial-world/mw-chapter-2256',
+                          'Afterword'), "Comment")
 
 if __name__ == '__main__':
     unittest.main()
